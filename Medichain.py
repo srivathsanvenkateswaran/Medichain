8464bdad9108394c0175340a517be4179fae972c
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2022-06-08 22:29:48 +0530

    Added Transaction feature

--- Medichain.py
+++ Medichain.py
@@ -5,7 +5,8 @@ import hashlib
 import json
 from textwrap import indent
 from urllib import response
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, session
+from flask_session import Session
 from numpy import block
 from paramiko import HostKeys
 
@@ -13,13 +14,14 @@ class Medichain:
     
     def __init__(self):
         self.chain = []
-        self.create_block(nonce = 1, prevHash = '0')
+        self.create_block(nonce = 1, prevHash = '0', records=[])
     
-    def create_block(self, nonce, prevHash):
+    def create_block(self, nonce, prevHash, records):
         block = {
             'index': len(self.chain) + 1,
             'timestamp': str(datetime.datetime.now()),
             'nonce': nonce,
+            'records': records,
             'previous_hash': prevHash
         }
         self.chain.append(block)
@@ -45,6 +47,9 @@ class Medichain:
         return hashlib.sha256(encodedBlock).hexdigest()
 
     def is_chain_valid(self, chain):
+        if(len(chain)<=1):
+            return True
+
         prevBlock = chain[0]
         for block in chain[1:]:
             if block['previous_hash'] != self.blockHash(prevBlock):
@@ -59,32 +64,72 @@ class Medichain:
 
 
 app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 blockchain = Medichain()
 
-@app.route('/mine_block', methods=['GET'])
-def mine_block():
+@app.route('/get_records', methods=['GET'])
+def get_records():
+    if 'memPool' not in session:
+        session['memPool'] = []
+
+    memPool = session['memPool']
+    return jsonify(memPool), 200
+
+@app.route('/add_record', methods=['GET', 'POST'])
+def add_record():
+    doctor = request.args.get('Doctor')
+    patient = request.args.get('Patient')
+    report = request.args.get('Report')
+
+    record = {
+            'Doctor': doctor,
+            'Patient': patient,
+            'Report': report
+        }
+
+    if 'memPool' not in session:
+        session['memPool'] = []
+
+    session['memPool'].append(record)
+    memPool = session['memPool']
+
+    if(len(memPool) == 5):
+        mine_block(memPool)
+        session['memPool'] = []
+
+    return jsonify(record), 200
+
+# @app.route('/mine_block', methods=['GET'])
+def mine_block(records):
     prevBlock = blockchain.get_previous_block()
     prevNonce = prevBlock["nonce"]
     currNonce = blockchain.proof_of_work(prevNonce)
     prevHash = blockchain.blockHash(prevBlock)
-    block = blockchain.create_block(currNonce, prevHash)
+    block = blockchain.create_block(currNonce, prevHash, records)
 
     response = {
         "message": 'Congratulation on mining a new block',
         'index': block['index'],
         'timestamp': block['timestamp'],
         'nonce': block['nonce'],
+        'records': records,
         'previous_hash': block['previous_hash']
     }
 
-    return jsonify(response), 200
+    # return jsonify(response)
 
 @app.route('/get_chain', methods=['GET'])
 def get_chain():
+    memPool = session['memPool']
+
     response = {
+        'chain_length': len(blockchain.chain),
         'chain': blockchain.chain,
-        'length': len(blockchain.chain)
+        'mem_pool_length': len(memPool),
+        'mem_pool': memPool
     }
 
     return jsonify(response), 200
